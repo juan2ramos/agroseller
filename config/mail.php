d850e82e7351841b42335d48d6a8ccfdbbe18639
Juan2Ramos <juan2ramos>
2016-02-10 12:14:55 -0500

    mail from

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-responder@agrosellers.com', 'name' => 'Agrosellers'],
 
     /*
     |--------------------------------------------------------------------------
