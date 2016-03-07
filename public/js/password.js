15d0ed190353fbc8e38fe55cd68d5c13d2c75434
Juan2Ramos <juan2ramos>
2016-03-07 11:57:08 -0500

    registro

--- public/js/password.js
+++ public/js/password.js
@@ -1,16 +1,21 @@
+var $pass = $('#passStrength');
 $('#password').keyup(function(e) {
     var strongRegex = /^.*(?=.{10,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\X])(?=.*[!$#%·?¿()&*]).*$/;
     var mediumRegex = /^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\X])(?=.*[!$#%·?¿()&*]).*$/;
     var enoughRegex = new RegExp("(?=.{6,}).*", "g");
 
     if (false == enoughRegex.test($(this).val())) {
-        $('#passstrength').html('Minímo 6 Caracteres');
+        $pass.css('background-color','#e08585');
+        $pass.css('width','20%');
     } else if (strongRegex.test($(this).val())) {
-        $('#passstrength').html('Fuerte!');
+        $pass.css('background-color','#75f09e');
+        $pass.css('width','90%');
     } else if (mediumRegex.test($(this).val())) {
-        $('#passstrength').html('Mediana!');
+        $pass.css('background-color','#f0a875');
+        $pass.css('width','60%');
     } else {
-        $('#passstrength').html('Débil!');
+        $pass.css('background-color','#e08585');
+        $pass.css('width','20%');
     }
     return true;
 
