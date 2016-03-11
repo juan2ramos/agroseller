ed3455ba7510bb87898b5f2ebdf094914c4f2ed2
Santiago Ruiz <sanruiz1003@gmail.com>
2016-03-11 17:13:43 -0500

    Model Factory Products

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,17 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+use Faker\Generator;
+use Agrosellers\User;
+use Agrosellers\Entities\Product;
 
-$factory->define(Agrosellers\User::class, function (Faker\Generator $faker) {
+$factory->define(User::class, function (Generator $faker) {
     return [
         'name' => $faker->name,
         'last_name' => $faker->name,
@@ -23,8 +16,29 @@ $factory->define(Agrosellers\User::class, function (Faker\Generator $faker) {
         'phone' => $faker->phoneNumber,
         'photo' => 'user.png',
         'role_id' => $faker->randomElement([2,3,4]),
-
     ];
 });
 
-
+$factory->define(Product::class, function(Generator $faker){
+    return [
+        'presentation' => $faker->randomElement(['Paquete', 'Bulto', 'Galon']),
+        'size' => $faker->randomElement(['Grande', 'Mediano', 'Pequeño']),
+        'weight' => $faker->randomFloat([10],[100]),
+        'measure' => $faker->randomElement(['Centimetros', 'Metros']),
+        'material' => $faker->randomElement(['Madera', 'Plastico', 'Metal']),
+        'description' => $faker->text($maxNbChars = 200),
+        'composition' => 'datos.pdf',
+        'forms-employment' => 'forms-employment',
+        'price' => $faker->numberBetween($min = 10000, $max = 10000000),
+        'taxes' => 'IVA',
+        'available-quantity' => $faker->numberBetween($min = 10000, $max = 10000000),
+        'image-scale' => $faker->imageUrl($width = 640, $height = 480),
+        'location' => $faker->country,
+        'offer' => $faker->numberBetween($min = 5, $max = 80),
+        'offer-price' => $faker->numberBetween($min = 10000, $max = 1000000),
+        'name' => $faker->firstName($gender = null|'male'|'female'),
+        'slug' => 'slug',
+        'subcategory_id' => $faker->numberBetween($min = 1, $max = 28),
+        'user_id' => 2,
+    ];
+});
