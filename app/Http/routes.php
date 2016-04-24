cd8cad036d1466a364909e141b38040210089aef
Juan2Ramos <juan2ramos>
2016-04-24 13:12:15 -0500

    login delete

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,6 +6,5 @@ Route::group(['middleware' => ['web']], function () {
     require __DIR__ . '/Routes/auth.php';
 });
 Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function () {
-    Auth::loginUsingId(11);
     require __DIR__ . '/Routes/admin.php';
 });
