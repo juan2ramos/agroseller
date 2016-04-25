be8af09bddaf00cf93e7906d6a2425f19d0a93af
santo52 <sanruiz1003@gmail.com>
2016-04-25 10:19:24 -0500

    Solucion fallas en back.product

--- resources/views/back/product.blade.php
+++ resources/views/back/product.blade.php
@@ -164,8 +164,7 @@
         });
     </script>
 @endsection
-@section('scripts')
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDbS0xs79_QKS4HFEJ_1PcT5bZYSBIByaA&signed_in=true&callback=initMap"
-            async defer></script>
-    <script src="{{asset('js/maps.js')}}"></script>
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/jquery.datetimepicker.css') }}">
+    <link rel="stylesheet" href="//cdn.quilljs.com/0.20.1/quill.snow.css"/>
 @endsection
