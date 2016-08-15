ec512e3a085beeb138af1a024b26627c12bc6283
santo52 <sanruiz1003@gmail.com>
2016-08-15 15:46:38 -0500

    Mensaje planes

--- resources/views/back/home.blade.php
+++ resources/views/back/home.blade.php
@@ -6,8 +6,7 @@
             <div class="MessagePlatform-content">
                 <span class="MessagePlatform-close">X</span>
                 <h2>Error en compra</h2>
-                <p>No puedes realizar la compra.</p>
-                <p>Debes loguearte o registrarte como proveedor.</p>
+                <p>No puedes realizar la compra. Debes loguearte o registrarte como proveedor.</p>
                 <p>Gracias por elegirnos.</p>
                 <p class="MessagePlatform-last">Centro de notificaciones <span>Agrosellers</span></p>
             </div>
