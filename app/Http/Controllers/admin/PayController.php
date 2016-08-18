675794e4b0395cf794c5a4210186fb04a0585145
santo52 <sanruiz1003@gmail.com>
2016-08-18 14:18:29 -0500

    Mejora final del plan

--- app/Http/Controllers/admin/PayController.php
+++ app/Http/Controllers/admin/PayController.php
@@ -4,6 +4,7 @@ namespace Agrosellers\Http\Controllers\admin;
 
 use Agrosellers\Entities\PlanProvider;
 use Agrosellers\Entities\Provider;
+use Faker\Provider\DateTime;
 use Illuminate\Http\Request;
 
 use Agrosellers\Http\Requests;
@@ -18,16 +19,27 @@ class PayController extends Controller
         if(auth()->user()->role_id == 3){
 
             // 2592000 1 mes
-            
-            PlanProvider::create([
-                'provider_id' => auth()->user()->provider->id,
-                'name' => $request->name,
-                'description' => $request->description,
-                'period' => $request->period,
-                'price' => $request->price
-            ]);
 
-            return redirect()->route('admin')->with(['message' => 'Estamos en proceso de aprobar su pedido. Pronto su asesor se comunicará con usted']);
+            $plan = PlanProvider::where('provider_id', auth()->user()->provider->id)->orderBy('created_at', 'DESC')->first();
+            $finalPlan = strtotime($plan->created_at) + $plan->period * 2592000;
+
+            if(strtotime(date('Y-m-d H:i:s')) > $finalPlan){
+            
+                PlanProvider::create([
+                    'provider_id' => auth()->user()->provider->id,
+                    'name' => $request->name,
+                    'description' => $request->description,
+                    'period' => $request->period,
+                    'price' => $request->price
+                ]);
+
+                $message = ['message' => 'Estamos en proceso de aprobar su pedido. Pronto su asesor se comunicará con usted'];
+            }
+            else {
+
+                $message = ['message' => "Usted ya ha comprado un plan. Este finaliza el día {$finalPlan}"];
+            }
+            return redirect()->route('admin')->with($message);
             //return view('back.payPlan', compact('plan'));
         }
         else{
