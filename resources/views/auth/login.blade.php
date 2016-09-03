27505370f34b9b8a4916610fd3a0461687a7c052
santo52 <sanruiz1003@gmail.com>
2016-09-03 15:08:04 -0500

    Login resposive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,42 @@
 @extends('layout')
 
 @section('content')
-    <header class="HeaderAuth">
-        <div class="HeaderAuth-content row middle ">
-            <figure class="col-6">
+    <header class="HeaderAuth Header-container">
+        <div class="HeaderAuth-content row middle">
+            <figure class="smaller-6">
                 <a href="/"><img src="{{ url('images/logo-agrosellers.svg') }}" alt=""></a>
             </figure>
-            <a class="col-6 end" href="{{route('register')}}">
-                <button class="Button">REGISTRATE</button>
-            </a>
+            <div class="Header-linksBack smaller-6 row end">
+                <a class="Register Button" href="{{route('register')}}">REGÍSTRATE</a>
+            </div>
         </div>
     </header>
 
+    <main class="Container">
+        <form class="row center middle FormAuth Forms Login" role="form" method="POST" action="{{ route('login') }}">
+            <div class="BackContainer FormAuth-container smaller-12">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <h2>INICIO DE SESIÓN</h2>
+                @if (!empty($errorArray["email"])) <span class="Login-error">{{ $errorArray["email"][0] }} </span>@endif
+                <label for="email">
+                    <input type="email" name="email" value="{{ old('email') }}">
+                    <span>E-mail</span>
+                </label>
 
+                @if (!empty($errorArray["password"])) <span
+                        class="Login-error">{{ $errorArray["password"][0] }} </span>
+                @endif
 
-    <form class="row center middle FormAuth Forms Login" role="form" method="POST" action="{{ route('login') }}">
-        <div class="BackContainer FormAuth-container">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <h2>INICIO DE SESIÓN</h2>
-            @if (!empty($errorArray["email"])) <span class="Login-error">{{ $errorArray["email"][0] }} </span>@endif
-            <label for="email">
-                <input type="email" name="email" value="{{ old('email') }}">
-                <span>E-mail</span>
-            </label>
+                <label for="password">
+                    <input type="password" name="password">
+                    <span>Contraseña</span>
+                </label>
+                <a class="login-restartPassword" href="/password/email">Olvido su contraseña?</a>
 
-            @if (!empty($errorArray["password"])) <span
-                    class="Login-error">{{ $errorArray["password"][0] }} </span>
-            @endif
-
-            <label for="password">
-                <input type="password" name="password">
-                <span>Contraseña</span>
-            </label>
-            <a class="login-restartPassword" href="/password/email">Olvido su contraseña?</a>
-
-            <button  type="submit" class="Button submit">INICIO DE SESIÓN</button>
-        </div>
-    </form>
+                <button  type="submit" class="Button submit">INICIO DE SESIÓN</button>
+            </div>
+        </form>
+    </main>
 
 @endsection
 @section('scripts')
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,8 @@
     <title>Agrosellers</title>
     <link rel="stylesheet" href="{{ asset('css/normalize.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
     <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700' rel='stylesheet' type='text/css'>
-
 </head>
 <body>
 @yield('content')
