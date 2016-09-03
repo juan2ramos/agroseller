6e7a7581d5cb710ec986c4758fbe041432c85d6d
santo52 <sanruiz1003@gmail.com>
2016-09-03 11:01:53 -0500

    Preguntas frecuentes responsive

--- resources/views/front/faqs.blade.php
+++ resources/views/front/faqs.blade.php
@@ -2,90 +2,39 @@
 
 @section('content')
    <div class="container">
-       <svg class="container_icon"
-               xmlns:dc="http://purl.org/dc/elements/1.1/"
-               xmlns:cc="http://creativecommons.org/ns#"
-               xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-               xmlns:svg="http://www.w3.org/2000/svg"
-               xmlns="http://www.w3.org/2000/svg"
-               version="1.1"
-               width="34"
-               height="34"
-               viewBox="0 0 34.000001 34.000001"
-               id="Capa_1"
-               xml:space="preserve"><metadata
-                   id="metadata3103"><rdf:RDF><cc:Work
-                           rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
-                               rdf:resource="http://purl.org/dc/dcmitype/StillImage" /><dc:title></dc:title></cc:Work></rdf:RDF></metadata><defs
-                   id="defs3101" /><g
-                   transform="matrix(0.73798965,0,0,0.73978973,3.9800124e-5,-0.04142822)"
-                   id="g3065"><path
-                       d="M 45.999,8.427 C 45.984,3.8 42.229,0.056 37.602,0.056 H 8.397 C 6.165,0.056 4.025,0.945 2.45,2.525 0.874,4.106 -0.008,6.25 0,8.481 l 0.071,22.284 c 0.015,4.628 3.77,8.29 8.398,8.29 h 8.294 l 3.922,5.705 c 0.529,0.76 1.396,1.253 2.321,1.255 0.925,0.002 1.793,-0.508 2.326,-1.265 l 3.969,-5.695 h 8.373 c 2.231,0 4.371,-0.807 5.947,-2.387 1.576,-1.582 2.458,-3.684 2.45,-5.916 L 45.999,8.427 z M 22.404,32.811 c -1.747,0 -2.934,-1.286 -2.934,-3 0,-1.748 1.22,-3.001 2.934,-3.001 1.781,0 2.936,1.253 2.968,3.001 0,1.713 -1.187,3 -2.968,3 z m 4.249,-14.076 c -1.331,1.475 -1.618,2.879 -1.618,4.499 0,1.203 -0.976,2.18 -2.179,2.18 h -0.897 c -1.191,0 -2.161,-0.957 -2.179,-2.146 l -0.003,-0.233 c -0.108,-1.836 0.504,-3.75 2.123,-5.693 1.152,-1.369 2.088,-2.543 2.088,-3.767 0,-1.26 -0.828,-2.099 -2.627,-2.171 -0.549,0 -1.152,0.092 -1.735,0.254 -1.068,0.298 -2.181,-0.307 -2.511,-1.367 -0.333,-1.068 0.251,-2.205 1.313,-2.557 1.163,-0.386 2.585,-0.658 4.156,-0.658 4.751,0 7.021,2.627 7.021,5.615 -0.001,2.732 -1.583,4.532 -2.952,6.044 z"
-                       id="path3067" /></g><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3069" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3071" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3073" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3075" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3077" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3079" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3081" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3083" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3085" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3087" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3089" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3091" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3093" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3095" /><g
-                   transform="translate(5.3935685e-5,0.02364778)"
-                   id="g3097" /><path
-                   d="m 16.356236,33.816481 c -0.678237,-0.234304 -0.647475,-0.198745 -2.494754,-2.883892 l -1.473264,-2.141491 -3.3921749,0 c -3.7616343,0 -4.0901886,-0.0312 -5.1609829,-0.490134 C 2.141826,27.575262 0.9575597,26.268024 0.37092303,24.47712 0.06929773,23.556307 0.04458738,22.668464 0.07001825,13.665611 0.09684283,4.169369 0.05572865,4.982747 0.5710684,3.7531375 1.2821645,2.05645 2.8032146,0.76272379 4.7229101,0.22179669 L 5.340277,0.04783657 16.375709,0.02211513 c 7.561455,-0.01762429 11.29414,6.5595e-4 11.857244,0.0580693 1.561759,0.15923451 2.80065,0.7319118 3.847024,1.77828567 0.765246,0.7652452 1.334284,1.7501979 1.643551,2.8448336 0.165557,0.5859839 0.16662,0.6396104 0.190572,9.6182593 0.02636,9.882265 0.03644,9.62744 -0.42351,10.708075 -0.743792,1.747506 -2.037697,2.915942 -3.918665,3.538678 -0.529557,0.17532 -0.613392,0.179368 -4.256595,0.205482 l -3.716398,0.02664 -1.529138,2.203635 c -0.905644,1.305122 -1.655849,2.306709 -1.839903,2.456427 -0.333199,0.27104 -0.917961,0.502624 -1.258805,0.498525 -0.115238,-0.0014 -0.39192,-0.06553 -0.61485,-0.142544 l 0,0 z m 1.261513,-9.791607 c 0.34547,-0.178882 0.726703,-0.574221 0.930331,-0.964754 0.163638,-0.313836 0.227331,-1.300022 0.111615,-1.728152 -0.331057,-1.224845 -1.710559,-1.888975 -2.950964,-1.420677 -1.072092,0.404753 -1.624276,1.631949 -1.267587,2.817136 0.20337,0.675749 0.631615,1.135989 1.313586,1.411731 0.373842,0.151155 1.480736,0.08266 1.863019,-0.115284 l 0,0 z m 0.03843,-5.445546 c 0.187062,-0.09876 0.437162,-0.317325 0.555775,-0.485705 0.188582,-0.267705 0.22918,-0.431687 0.323329,-1.305979 0.166237,-1.543714 0.371028,-1.999471 1.485625,-3.306225 1.198993,-1.405702 1.650974,-2.290438 1.808907,-3.5408756 C 22.11063,7.7172272 20.851204,5.9827343 18.497907,5.3518043 17.867043,5.1826666 15.548758,5.161878 14.832292,5.3189339 c -0.905894,0.1985802 -1.501844,0.4055297 -1.736575,0.603042 -0.755945,0.636086 -0.709003,1.9461736 0.08865,2.4740338 0.396885,0.2626456 0.86249,0.2997887 1.785992,0.1424751 1.032526,-0.1758854 1.828669,-0.021626 2.274623,0.4407274 0.295872,0.3067518 0.404048,0.5811706 0.404048,1.0249788 0,0.665994 -0.342698,1.291458 -1.498739,2.735369 -1.088523,1.359581 -1.588212,2.63616 -1.585859,4.05147 0.0013,0.778625 0.142171,1.203653 0.515038,1.55387 0.354733,0.333185 0.619095,0.407714 1.457738,0.410966 0.67,0.0026 0.826402,-0.02208 1.118977,-0.176538 l 0,0 z"
-                   id="path3105"
-                   style="fill:#C5D257;stroke:#C5D257" />
+       <svg width="34" height="34" class="container_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.000001 34.000001">
+           <path d="m 16.356236,33.816481 c -0.678237,-0.234304 -0.647475,-0.198745 -2.494754,-2.883892 l -1.473264,-2.141491 -3.3921749,0 c -3.7616343,0 -4.0901886,-0.0312 -5.1609829,-0.490134 C 2.141826,27.575262 0.9575597,26.268024 0.37092303,24.47712 0.06929773,23.556307 0.04458738,22.668464 0.07001825,13.665611 0.09684283,4.169369 0.05572865,4.982747 0.5710684,3.7531375 1.2821645,2.05645 2.8032146,0.76272379 4.7229101,0.22179669 L 5.340277,0.04783657 16.375709,0.02211513 c 7.561455,-0.01762429 11.29414,6.5595e-4 11.857244,0.0580693 1.561759,0.15923451 2.80065,0.7319118 3.847024,1.77828567 0.765246,0.7652452 1.334284,1.7501979 1.643551,2.8448336 0.165557,0.5859839 0.16662,0.6396104 0.190572,9.6182593 0.02636,9.882265 0.03644,9.62744 -0.42351,10.708075 -0.743792,1.747506 -2.037697,2.915942 -3.918665,3.538678 -0.529557,0.17532 -0.613392,0.179368 -4.256595,0.205482 l -3.716398,0.02664 -1.529138,2.203635 c -0.905644,1.305122 -1.655849,2.306709 -1.839903,2.456427 -0.333199,0.27104 -0.917961,0.502624 -1.258805,0.498525 -0.115238,-0.0014 -0.39192,-0.06553 -0.61485,-0.142544 l 0,0 z m 1.261513,-9.791607 c 0.34547,-0.178882 0.726703,-0.574221 0.930331,-0.964754 0.163638,-0.313836 0.227331,-1.300022 0.111615,-1.728152 -0.331057,-1.224845 -1.710559,-1.888975 -2.950964,-1.420677 -1.072092,0.404753 -1.624276,1.631949 -1.267587,2.817136 0.20337,0.675749 0.631615,1.135989 1.313586,1.411731 0.373842,0.151155 1.480736,0.08266 1.863019,-0.115284 l 0,0 z m 0.03843,-5.445546 c 0.187062,-0.09876 0.437162,-0.317325 0.555775,-0.485705 0.188582,-0.267705 0.22918,-0.431687 0.323329,-1.305979 0.166237,-1.543714 0.371028,-1.999471 1.485625,-3.306225 1.198993,-1.405702 1.650974,-2.290438 1.808907,-3.5408756 C 22.11063,7.7172272 20.851204,5.9827343 18.497907,5.3518043 17.867043,5.1826666 15.548758,5.161878 14.832292,5.3189339 c -0.905894,0.1985802 -1.501844,0.4055297 -1.736575,0.603042 -0.755945,0.636086 -0.709003,1.9461736 0.08865,2.4740338 0.396885,0.2626456 0.86249,0.2997887 1.785992,0.1424751 1.032526,-0.1758854 1.828669,-0.021626 2.274623,0.4407274 0.295872,0.3067518 0.404048,0.5811706 0.404048,1.0249788 0,0.665994 -0.342698,1.291458 -1.498739,2.735369 -1.088523,1.359581 -1.588212,2.63616 -1.585859,4.05147 0.0013,0.778625 0.142171,1.203653 0.515038,1.55387 0.354733,0.333185 0.619095,0.407714 1.457738,0.410966 0.67,0.0026 0.826402,-0.02208 1.118977,-0.176538 l 0,0 z" style="fill:#C5D257;stroke:#C5D257" />
        </svg>
        <h1>Preguntas frecuentes</h1>
        <span>Atedemos tus consultas</span>
        <section class="container_questions">
            <section class="container_questions_item">
-               <ol>
-                   <li class="question">
+               <ol class="row" style="padding:0">
+                   <li class="question smaller-12">
                        <span>¿Como realizo la compra?</span>
                        <article class="text">
                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolore eveniet excepturi, in libero modi nihil nulla odio odit possimus quidem sapiente. Animi asperiores est eum exercitationem impedit, maiores natus pariatur quos ratione repellendus sint tempore veniam! A alias consequatur corporis eum, facilis, ipsam iure molestiae molestias nisi quidem quod sapiente ut? Accusamus delectus eos libero nisi voluptatem voluptates! A accusamus odio quasi sapiente veniam voluptate. Animi dignissimos dolorem doloremque dolores ducimus, ex excepturi exercitationem inventore nihil pariatur quae quos repellat tempora temporibus unde veniam veritatis voluptates. Aperiam corporis debitis dolores ducimus, fugit harum illum molestiae reprehenderit saepe sequi velit.</p>
                        </article>
                    </li>
-                   <li class="question">
+                   <li class="question smaller-12">
                        <span>¿Como garantizan mi compra?</span>
                        <article class="text">
                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolore eveniet excepturi, in libero modi nihil nulla odio odit possimus quidem sapiente. Animi asperiores est eum exercitationem impedit, maiores natus pariatur quos ratione repellendus sint tempore veniam! A alias consequatur corporis eum, facilis, ipsam iure molestiae molestias nisi quidem quod sapiente ut? Accusamus delectus eos libero nisi voluptatem voluptates! A accusamus odio quasi sapiente veniam voluptate. Animi dignissimos dolorem doloremque dolores ducimus, ex excepturi exercitationem inventore nihil pariatur quae quos repellat tempora temporibus unde veniam veritatis voluptates. Aperiam corporis debitis dolores ducimus, fugit harum illum molestiae reprehenderit saepe sequi velit.</p>
                        </article>
                    </li>
-                   <li class="question">
+                   <li class="question smaller-12">
                        <span>¿Como administro la informacion de mis clientes ?</span>
                        <article class="text">
                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolore eveniet excepturi, in libero modi nihil nulla odio odit possimus quidem sapiente. Animi asperiores est eum exercitationem impedit, maiores natus pariatur quos ratione repellendus sint tempore veniam! A alias consequatur corporis eum, facilis, ipsam iure molestiae molestias nisi quidem quod sapiente ut? Accusamus delectus eos libero nisi voluptatem voluptates! A accusamus odio quasi sapiente veniam voluptate. Animi dignissimos dolorem doloremque dolores ducimus, ex excepturi exercitationem inventore nihil pariatur quae quos repellat tempora temporibus unde veniam veritatis voluptates. Aperiam corporis debitis dolores ducimus, fugit harum illum molestiae reprehenderit saepe sequi velit.</p>
                        </article>
                    </li>
-                   <li class="question">
+                   <li class="question smaller-12">
                        <span>¿Que riezgos corro durante la compra de un producto?</span>
                        <article class="text">
                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolore eveniet excepturi, in libero modi nihil nulla odio odit possimus quidem sapiente. Animi asperiores est eum exercitationem impedit, maiores natus pariatur quos ratione repellendus sint tempore veniam! A alias consequatur corporis eum, facilis, ipsam iure molestiae molestias nisi quidem quod sapiente ut? Accusamus delectus eos libero nisi voluptatem voluptates! A accusamus odio quasi sapiente veniam voluptate. Animi dignissimos dolorem doloremque dolores ducimus, ex excepturi exercitationem inventore nihil pariatur quae quos repellat tempora temporibus unde veniam veritatis voluptates. Aperiam corporis debitis dolores ducimus, fugit harum illum molestiae reprehenderit saepe sequi velit.</p>
                        </article>
                    </li>
-                   <li class="question">
+                   <li class="question smaller-12">
                        <span>¿Cual es el proceso para realizar una venta?</span>
                        <article class="text">
                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolore eveniet excepturi, in libero modi nihil nulla odio odit possimus quidem sapiente. Animi asperiores est eum exercitationem impedit, maiores natus pariatur quos ratione repellendus sint tempore veniam! A alias consequatur corporis eum, facilis, ipsam iure molestiae molestias nisi quidem quod sapiente ut? Accusamus delectus eos libero nisi voluptatem voluptates! A accusamus odio quasi sapiente veniam voluptate. Animi dignissimos dolorem doloremque dolores ducimus, ex excepturi exercitationem inventore nihil pariatur quae quos repellat tempora temporibus unde veniam veritatis voluptates. Aperiam corporis debitis dolores ducimus, fugit harum illum molestiae reprehenderit saepe sequi velit.</p>
@@ -95,4 +44,12 @@
            </section>
        </section>
    </div>
+@endsection
+@section('scripts')
+    <script>
+        var $question = $('.container_questions_item .question span');
+        $question.on('click', function () {
+            $(this).siblings().slideToggle();
+        });
+    </script>
 @endsection
