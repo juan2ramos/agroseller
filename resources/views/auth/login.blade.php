f6e7401a5a620bd940e2362b1bc0528cede775d0
santo52 <sanruiz1003@gmail.com>
2016-09-03 15:12:59 -0500

    Registro responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,33 +11,29 @@
             </div>
         </div>
     </header>
+    <form class="row center middle FormAuth Forms Login" role="form" method="POST" action="{{ route('login') }}">
+        <div class="BackContainer FormAuth-container smaller-12">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <h2>INICIO DE SESIÓN</h2>
+            @if (!empty($errorArray["email"])) <span class="Login-error">{{ $errorArray["email"][0] }} </span>@endif
+            <label for="email">
+                <input type="email" name="email" value="{{ old('email') }}">
+                <span>E-mail</span>
+            </label>
 
-    <main class="Container">
-        <form class="row center middle FormAuth Forms Login" role="form" method="POST" action="{{ route('login') }}">
-            <div class="BackContainer FormAuth-container smaller-12">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <h2>INICIO DE SESIÓN</h2>
-                @if (!empty($errorArray["email"])) <span class="Login-error">{{ $errorArray["email"][0] }} </span>@endif
-                <label for="email">
-                    <input type="email" name="email" value="{{ old('email') }}">
-                    <span>E-mail</span>
-                </label>
+            @if (!empty($errorArray["password"]))
+                <span class="Login-error">{{ $errorArray["password"][0] }} </span>
+            @endif
 
-                @if (!empty($errorArray["password"])) <span
-                        class="Login-error">{{ $errorArray["password"][0] }} </span>
-                @endif
+            <label for="password">
+                <input type="password" name="password">
+                <span>Contraseña</span>
+            </label>
+            <a class="login-restartPassword" href="/password/email">Olvido su contraseña?</a>
 
-                <label for="password">
-                    <input type="password" name="password">
-                    <span>Contraseña</span>
-                </label>
-                <a class="login-restartPassword" href="/password/email">Olvido su contraseña?</a>
-
-                <button  type="submit" class="Button submit">INICIO DE SESIÓN</button>
-            </div>
-        </form>
-    </main>
-
+            <button  type="submit" class="Button submit">INICIO DE SESIÓN</button>
+        </div>
+    </form>
 @endsection
 @section('scripts')
     <script src="{{asset('js/forms.js')}}"></script>
