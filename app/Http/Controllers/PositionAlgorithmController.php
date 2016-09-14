218d7b2f9568ab07b3e21e9369ace71205533c73
juan2ramos <juan2ramos@gmail.com>
2016-09-14 15:36:38 -0500

    distance

--- app/Http/Controllers/PositionAlgorithmController.php
+++ app/Http/Controllers/PositionAlgorithmController.php
@@ -24,12 +24,12 @@ class PositionAlgorithmController extends Controller
         foreach ($products as $product) {
             $i = $i + 1;
             $product->location2 = explode(';', $product->location);
-            $product->distance = $this->distance($lat,$lng,$product->location2);
+            $product->distance = $this->distance($lat, $lng, $product->location2);
 
             //$collection->push(['farms' => $product->farms, 'distance' => $distance, 'id' => $product->id]);
         }
 
-        $sorted = $products->sortBy(function ($p, $key) {
+        /*$sorted = $products->sortBy(function ($p, $key) {
             $m[-1][-1] = -1;
             $m[-1][0] = 0;
             $m[-1][1] = 1;
@@ -41,8 +41,19 @@ class PositionAlgorithmController extends Controller
             $m[1][1] = 1;
             return $m[$this->distancePriority($p['distance'])][$this->farms($p['farms'])];
 
+        });*/
+        $sorted = $products->sortBy(function ($p, $key) {
+            return $this->distancePriority($p['distance']);
         });
+     /*    foreach($sorted as $p){
 
+             echo 'Producto: ';print_r($p->id);echo '<br>';
+             echo " &#31; &#31; &#31; &#31; nombre: ";print_r($p->name);echo '<br>';
+             echo " &#31; &#31; &#31; &#31; cordenadas: ";print_r($p->location);echo '<br>';
+             echo " &#31; &#31; &#31; &#31; cordenadas 2: ";print_r($p->location2);echo '<br>';
+             echo " &#31; &#31; &#31; &#31; distancia: ";print_r($p->distance );echo '<br>';
+         }
+        dd();*/
 
         return $sorted;
     }
@@ -52,29 +63,31 @@ class PositionAlgorithmController extends Controller
         return 0;
         if (!Auth::check() || empty($farms))
             return 1;
-            $farmsArray = explode(',' , $farms);
+        $farmsArray = explode(',', $farms);
         return $farms;
     }
+
     private function distancePriority($distance)
     {
-        return $distance < 100? -1 : ( $distance < 400 ? 0 : 1);
+        return $distance < 10 ? -1 : ($distance < 50 ? 0 : 1);
     }
-    private function distance($lat,$lng,$locations )
+
+    private function distance($lat, $lng, $locations)
     {
         $dists = [];
-        foreach($locations as $location ){
+        foreach ($locations as $location) {
             $locationArray = explode('&', $location);
-            if(!is_numeric($locationArray[0]) || !is_numeric($locationArray[1]) || empty($locationArray) )  {
+            if (!is_numeric($locationArray[0]) || !is_numeric($locationArray[1]) || empty($locationArray)) {
                 continue;
             }
 
             $dists[] = 6371 *
-             acos(
-                cos(deg2rad($lat)) *
-                cos(deg2rad($locationArray[1])) *
-                cos(deg2rad($locationArray[0]) - deg2rad($lng)) +
-                sin(deg2rad($lat)) * sin(deg2rad($locationArray[1]))
-            );
+                acos(
+                    cos(deg2rad($lat)) *
+                    cos(deg2rad($locationArray[1])) *
+                    cos(deg2rad($locationArray[0]) - deg2rad($lng)) +
+                    sin(deg2rad($lat)) * sin(deg2rad($locationArray[1]))
+                );
 
         }
 
