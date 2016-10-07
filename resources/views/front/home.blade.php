d90ec9be1f4952fcf2d5a0b9a13e62882d7c685e
juan2ramos <juan2ramos@gmail.com>
2016-10-07 15:56:19 -0500

    banner

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -35,7 +35,15 @@
         <div class="smaller-12 col-9" id="Banner">
           <div>
               <img src="{{url('images/agrosellers-banner.jpg')}}" alt="">
-            
+              <div class="BannerTop-info row middle">
+                  <div class="smaller-12 medium-6">
+                      <h2>CONSULTORÍAS Y ASESORÍAS </h2>
+                      <h3>TECNOLOGÍA AGRÍCOLA</h3>
+                      <!--<p class="BannerTop-infoPrice"><span>$80.000.000</span> Ahora $50.000.000</p>-->
+                  </div>
+                  <div class="smaller-12 medium-6 row end" style="padding: 10px 0;"><a class="Button" href="#">VER
+                          OFERTA</a></div>
+              </div>
 
           </div>
             <div>
