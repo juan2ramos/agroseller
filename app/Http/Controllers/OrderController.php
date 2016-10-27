6a09e15a6ab7100f3a886fc4c67fe9a99e052436
santo52 <sanruiz1003@gmail.com>
2016-10-27 15:00:28 -0500

    modificación de pago PSE

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,6 @@
 namespace Agrosellers\Http\Controllers;
 
 use Agrosellers\Entities\Order;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Agrosellers\Services\ZonaPagos;
 
@@ -14,45 +13,12 @@ class OrderController extends Controller
 {
     public function add(Request $request)
     {
-        if (empty($cart = Session::get('cart')))
+        if (empty(Session::get('cart')))
             return back();
 
         $inputs = $request->all();
         $zp = ZonaPagos::create();
         $id = $zp->invoiceRequest($inputs);
-
-        $data = [];
-        foreach ($cart as $item) {
-            // Temporal  state_order_id Zona Pagos
-            if(!$item->offers){
-                $value = $item->price;
-            }
-            else{
-                $value = (Carbon::now()->between(new Carbon($item->offers->offer_on), new Carbon($item->offers->offer_off)))
-                    ? $item->offers->offer_price
-                    : $item->price;
-            }
-
-            $data[$item->id] = ['quantity' => $item->quantity, 'state_order_id' => 2, 'value' => $value];
-        }
-
-        $order = Order::create([
-            'user_id' => auth()->user()->id,
-            'description' => $inputs['descripcion_pago'],
-            'name_client' => $inputs['nombre_cliente'] . ' ' . $inputs['apellido_cliente'],
-            'identification_client' => $inputs['id_cliente'],
-            'address_client' => $inputs['info_opcional1'],
-            'phone' => $inputs['telefono_cliente'],
-            'zp_buy_id' => $inputs['id_pago'],
-            'zp_buy_token' => $id,
-            'zp_state' => '888'
-        ]);
-
-        auth()->user()->orders()->save($order);
-        $order->products()->attach($data);
-
-        Session::forget('cart');
-        Session::forget('valueTotal');
 
         return redirect()->to("https://www.zonapagos.com/" . env('ZP_ROUTE_CODE') . "/pago.asp?estado_pago=iniciar_pago&identificador=" . $id);
     }
