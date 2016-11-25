51853e1accf818a20c6d503fd024fb3b2efa0b78
Juan2Ramos <juan2ramos@gmail.com>
2016-11-24 21:42:36 -0500

    caja eliminar

--- resources/views/back/partial/step2.blade.php
+++ resources/views/back/partial/step2.blade.php
@@ -61,7 +61,7 @@
         <option value="">Selecione la presentación</option>
         <option value="Bolsa" {{(isset($productEdit) && $productEdit->presentation == 'Bolsa')?'selected':''}}>Bolsa</option>
         <option value="Bulto" {{(isset($productEdit) && $productEdit->presentation == 'Bulto')?'selected':''}}>Bulto</option>
-        <option value="envase-plastico" {{(isset($productEdit) && $productEdit->presentation == 'envase-plastico')?'selected':''}}>Caja</option>
+        <option value="envase-plastico" {{(isset($productEdit) && $productEdit->presentation == 'envase-plastico')?'selected':''}}>Plastico</option>
         <option value="envase-vidrio" {{(isset($productEdit) && $productEdit->presentation == 'envase-vidrio')?'selected':''}}>Vidrio</option>
         <option value="Caja" {{(isset($productEdit) && $productEdit->presentation == 'Caja')?'selected':''}}>Caja</option>
         <option value="sobre" {{(isset($productEdit) && $productEdit->presentation == 'sobre')?'selected':''}}>Sobre</option>
