0c936ad6ce7ea280411d7546d1ffa03e6aa09e27
Juan2Ramos <juan2ramos@gmail.com>
2016-11-24 22:42:04 -0500

    desactivar pedidos y preguntas temporal

--- resources/views/back/partial/menu.blade.php
+++ resources/views/back/partial/menu.blade.php
@@ -2,12 +2,19 @@
 <nav class="Nav col-2">
     <ul>
         @foreach($menu->getMenu() as $key => $link)
-            <li>
+            @if($link['route'] == 'orderShowProvider' || $link['route'] == 'questions')
+                <a class="" style="    opacity: 0.3;" href="#">
+                    <figure>
+                        <img src="{{asset('images/'.$link['class'].'.svg')}}" alt="">
+                    </figure>
+                    {{ $key }}
+                </a>
+            @else
                 <a class="" href="{{ route($link['route']) }}">
                     <figure><img src="{{asset('images/'.$link['class'].'.svg')}}" alt=""></figure>
                     {{ $key }}
                 </a>
-            </li>
+            @endif
         @endforeach
     </ul>
 </nav>
