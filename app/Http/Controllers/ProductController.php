777755840a815a8e0cb8e100f85ffe69f8504760
Juan ramos <juan2ramos@MacBook-Pro-de-Juan.local>
2017-02-06 11:27:58 -0500

    is active

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -70,6 +70,7 @@ class ProductController extends Controller
         }
         $productProvider = ProductProvider::with('packing')->whereIn('id',$ids)->where('isActive', 1)->get()->toJson();
 
+        dd($productProvider);
         $questions = $this->reloadQuestions($id);
         $featuresTranslate = $this->setFeaturesTranslate($product);
         $cities = City::lists('nombre_ciudad', 'id_ciudad');
