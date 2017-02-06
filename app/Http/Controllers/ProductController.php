83102d357f1846efe96fa35ec523cc6dbb822afe
Juan ramos <juan2ramos@MacBook-Pro-de-Juan.local>
2017-02-06 11:24:56 -0500

    is active

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -69,7 +69,7 @@ class ProductController extends Controller
             $ids[] = $provider->pivot->id;
         }
         $productProvider = ProductProvider::with('packing')->whereIn('id',$ids)->where('isActive', 1)->get()->toJson();
-º
+
         dd($productProvider);
         $questions = $this->reloadQuestions($id);
         $featuresTranslate = $this->setFeaturesTranslate($product);
